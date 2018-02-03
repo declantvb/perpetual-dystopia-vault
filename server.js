086c24fa857439203230df660b3e8aa59c0cdcbd
Declan ter Veer-Burke <declantvb@gmail.com>
2018-02-03 16:58:18 +1300

    moved libraries to lib folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/assets/', express.static(__dirname + '/assets'));
+app.use('/lib/', express.static(__dirname + '/lib'));
 
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
